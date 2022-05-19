09b45140a829ae450e1630df004076f2f3c0a8c3
Lingfan Gao <lingfangao@hotmail.com>
2022-05-19 14:48:18 +0200

    demo app

--- packages/webpack-example/src/index.tsx
+++ packages/webpack-example/src/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { context } from './context';
-import { context as globalContext } from './globalContext';
+import { ProviderContext as ProviderContextV1 } from './fake_node_modules/context-v1.0.0';
+import { ProviderContext as ProviderContextV11 } from './fake_node_modules/context-v1.1.0';
+import { ProviderContext as ProviderContextV2 } from './fake_node_modules/context-v2.0.0';
 
 const root = document.getElementById('root');
 
-function App() {
-  const contextValue = React.useContext(context);
-  const globalContextValue = React.useContext(globalContext);
+function Example() {
+  const ctx = React.useContext(ProviderContextV1);
+  const ctxV11 = React.useContext(ProviderContextV11);
+  const ctxV2 = React.useContext(ProviderContextV2);
   return (
     <>
-      <div>{contextValue}</div>
-      <div>{globalContextValue}</div>
+      <pre>1.0.0: {JSON.stringify(ctx)}</pre>
+      <pre>1.1.0: {JSON.stringify(ctxV11)}</pre>
+      <pre>2.0.0: {JSON.stringify(ctxV2)}</pre>
     </>
+  );
+
+}
+
+function App() {
+  return (
+    <ProviderContextV1.Provider value={{ foo: 'red', bar: 'blue' }}>
+      <ProviderContextV2.Provider value={{bar: 'white', baz: 'black'}}>
+        <Example />
+      </ProviderContextV2.Provider>
+    </ProviderContextV1.Provider>
   );
 }
 
